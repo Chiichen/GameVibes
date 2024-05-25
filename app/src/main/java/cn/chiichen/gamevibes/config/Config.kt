65b0f87bdbb5e71916654e160a5cbe6711662a82
chiichen <chiichen@qq.com>
2024-05-25 17:49:17 +0800

    feat: [WIP] impl login webview

---
+++ app/src/main/java/cn/chiichen/gamevibes/config/Config.kt
@@ -0,0 +1,13 @@
+package cn.chiichen.gamevibes.config
+
+import org.casdoor.CasdoorConfig
+
+class GameVibesConfig {
+    companion object{
+        const val casdoor_endpoint = "https://auth.chiichen.cn"
+        const val casdoor_clientID = "e605bd789a0836f22186"
+        const val casdoor_organizationName = "GameVibes"
+        const val casdoor_redirectUri = "casdoor://callback"
+        const val casdoor_appName = "GameVibes"
+    }
+}
--- app/src/main/java/cn/chiichen/gamevibes/ui/common/login/LoginWebviewScreen.kt
+++ app/src/main/java/cn/chiichen/gamevibes/ui/common/login/LoginWebviewScreen.kt
@@ -1,6 +1,7 @@
 package cn.chiichen.gamevibes.ui.common.login
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.webkit.WebSettings
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,17 +11,32 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import cn.chiichen.gamevibes.R
+import cn.chiichen.gamevibes.config.GameVibesConfig
 import cn.chiichen.gamevibes.ui.common.webview.CustomWebView
+import org.casdoor.Casdoor
+import org.casdoor.CasdoorConfig
+
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun LoginWebview(){
-
+    val casdoorConfig = CasdoorConfig(
+        endpoint = GameVibesConfig.casdoor_endpoint,
+        clientID = GameVibesConfig.casdoor_clientID,
+        organizationName = GameVibesConfig.casdoor_organizationName,
+        redirectUri = GameVibesConfig.casdoor_redirectUri,
+        appName = GameVibesConfig.casdoor_appName,
+    )
+    Log.i("Login", String.format("LoginWebview: endpoint %s",casdoorConfig.endpoint))
+    val casdoor = Casdoor(casdoorConfig)
+    val loginUrl = casdoor.getSignInUrl(scope = "profile")
+    Log.i("Login", String.format("LoginWebview: loginUrl %s",loginUrl))
     var rememberWebViewProgress: Int by remember { mutableIntStateOf(-1) }
     Box {
         CustomWebView(
             modifier = Modifier.fillMaxSize(),
-            url = "https://www.baidu.com/",
+            url = loginUrl,
             onProgressChange = { progress ->
                 rememberWebViewProgress = progress
             },
@@ -45,7 +61,8 @@ fun LoginWebview(){
                 if (webView?.canGoBack() == true) {
                     webView.goBack()
                 }
-            }, onReceivedError = {
+            }, onReceivedError = {error ->
+                Log.e("Login", String.format("LoginWebview error %s",error) )
             }
         )
     }
