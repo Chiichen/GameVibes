8a1eed90b93c95ace38ccc34107a81d0ae924598
cc0924 <92106612+H0924cc@users.noreply.github.com>
2024-06-15 12:27:59 +0800

    feat: [WIP]navigation and homescreen (#1)

---
+++ app/src/main/java/cn/chiichen/gamevibes/ui/common/games/GamesScreen.kt
@@ -0,0 +1,13 @@
+package cn.chiichen.gamevibes.ui.common.games
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GamesScreen() {
+    Text(text = "games")
+}
