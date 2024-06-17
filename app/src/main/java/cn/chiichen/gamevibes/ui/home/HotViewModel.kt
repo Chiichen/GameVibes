35054ad082c98d9dd83f98196342d6f57baf9271
H0924cc <2530221402@qq.com>
2024-06-17 23:22:44 +0800

    change the agp version from 8.5.0 to 8.3.0

--- app/src/main/java/cn/chiichen/gamevibes/ui/home/HotViewModel.kt
+++ app/src/main/java/cn/chiichen/gamevibes/ui/home/HotViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cn.chiichen.gamevibes.model.entities.Article
-import cn.chiichen.gamevibes.model.entities.loadMoreArticles
 import cn.chiichen.gamevibes.model.response.ArticleResponse
 import cn.chiichen.gamevibes.network.RetrofitClient
 import kotlinx.coroutines.flow.MutableStateFlow
