9f34cdf9c230b2ca9d485b16b1d003c349ae6d0f
Chiichen <chiichen@qq.com>
2024-06-26 23:47:35 +0800

    chore:remove unused method
    Signed-off-by: Chiichen <chiichen@qq.com>

--- app/src/main/java/cn/chiichen/gamevibes/repository/UserRepository.kt
+++ app/src/main/java/cn/chiichen/gamevibes/repository/UserRepository.kt
@@ -1,34 +1,13 @@
 package cn.chiichen.game
 
 
-import cn.chiichen.gamevibes.model.entities.AuthToken
 import cn.chiichen.gamevibes.model.entities.UserProfile
-import cn.chiichen.gamevibes.model.request.TokenDetailRequest
-import cn.chiichen.gamevibes.network.RetrofitClient.authApiService
 import cn.chiichen.gamevibes.network.RetrofitClient.userApiService
 import com.tencent.mmkv.MMKV
 import retrofit2.awaitResponse
-import java.time.Instant
 
 class UserRepository {
     private val mmkv: MMKV = MMKV.defaultMMKV()
-
-    /// 验证token是否有效
-    /// @return  True为有效，False为无效是否有效
-    suspend fun getToken(): AuthToken? {
-        val token = mmkv.decodeString("login_token") ?: return null
-        val tokenDetailRequest = TokenDetailRequest(code = token)
-        val response =
-            authApiService.getTokenDetail(tokenDetailRequest).awaitResponse().body() ?: return null
-        val authToken = AuthToken(
-            response.access_token,
-            response.id_token,
-            response.refresh_token,
-            response.token_type, Instant.now().plusSeconds(response.expires_in.toLong()),
-            response.scope
-        )
-        return authToken
-    }
 
     suspend fun getUserProfile(token: String): UserProfile? {
         val response = userApiService.getUserProfile(token).awaitResponse()
